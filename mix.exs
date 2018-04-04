0268aadc884692495647a4c949402e248fd537ce
Dax Ravi <d@ironbay.co>
2018-04-04 17:57:14 -0400

    Dropped version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hub.MixProject do
     [
       app: :hub,
       version: "0.1.0",
-      elixir: "~> 1.7-dev",
+      elixir: ">= 1.6.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
